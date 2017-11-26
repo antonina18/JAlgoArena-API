24497c8b74c27929b00cd2febbcfcac27782f044
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-11-26 16:13:06 +0100

    add swagger

--- src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
+++ src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
@@ -1,23 +1,29 @@
 package com.jalgoarena
 
 import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Bean
-import org.springframework.boot.CommandLineRunner
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 @Configuration
+@EnableSwagger2
 open class JAlgoArenaApiGatewayApplication {
 
     private val LOG = LoggerFactory.getLogger(this.javaClass)
@@ -30,6 +36,15 @@ open class JAlgoArenaApiGatewayApplication {
                             LOG.info("${route.id} (${route.location}) ${route.fullPath}")
                         }
             }
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
 
     @Bean
     open fun corsFilter(): CorsFilter {
